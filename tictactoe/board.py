14f72ad6161527216de3912cea06d7470f3724dd
JamDiet <eschbachjonathan1@gmail.com>
2025-10-02 00:53:55 -0400

    Commented methods

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -5,6 +5,7 @@ class Board():
         self.available_moves = [[3*i+1, 3*i+2, 3*i+3] for i in range(3)]
 
     def get_move(self):
+        """Retrieve move input from user."""
         while True:
             try:
                 for i in range(3):
@@ -19,6 +20,7 @@ class Board():
                 print('Invalid choice. Please try again.')
 
     def update(self, player: int, move: int):
+        """Update board in place with player's symbol at the indicated move location."""
         row_idx = (move - 1) // 3
         col_idx = (move - 1) % 3
 
@@ -26,9 +28,11 @@ class Board():
             self.board[row_idx][col_idx] = 'X'
         else:
             self.board[row_idx][col_idx] = 'O'
+
         self.available_moves[row_idx][col_idx] = '_'
     
     def get_win_combos(self):
+        """Retrieve all potential win states from board."""
         return [self.board[0],
                 self.board[1],
                 self.board[2],
@@ -41,12 +45,14 @@ class Board():
     def check_for_win(self):
         if any(combo in self.winners for combo in self.get_win_combos()):
             return True
+        
         return False
     
     def check_for_stalemate(self):
         for i in range(3):
             if any(element != '_' for element in self.available_moves[i]):
                 return False
+            
         return True
     
     def print_board(self):
